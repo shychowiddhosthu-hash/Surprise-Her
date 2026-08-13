1d9b7b371acc7f88ba1f7cbfe0e7202ad29817d6
shychowiddhosthu-hash <shycho.wid.dhosthu@gmail.com>
2026-08-14 00:52:00 +0530

    Delete ja directory

--- ja/local-api.js
+++
@@ -1,5 +0,0 @@
-window.birthdayAPI = window.birthdayAPI || {
-  getWebsiteIdFromURL(){ return null; },
-  async getBirthdayWebsiteByWebsiteId(){ return {success:false}; },
-  createShareableURL(id){ return window.location.href; }
-};
--- ja/local-lang.js
+++
@@ -1,5 +0,0 @@
-window.t = window.t || function(key, vars){
-  let s = String(key);
-  if (vars) for (const [k,v] of Object.entries(vars)) s=s.replaceAll(`{${k}}`, v);
-  return s;
-};
